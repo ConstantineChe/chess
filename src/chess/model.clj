fd168263e10832143b7d6f338c990e9bd166b5d0
Konstantyn Cherkasyn <kcherkashyn@ebay.com>
2015-11-05 17:29:28 +0200

    structure experiments

--- src/chess/model.clj
+++ src/chess/model.clj
@@ -20,17 +20,29 @@
 
 (def board
   (atom
-   (repeatedly 8 (fn []
-                   (let [n (switch-turn)]
-                     (vec (repeatedly 8 (fn [] (switch-turn))))
-                     )))))
+   (vec (repeatedly 8 (fn []
+                     (let [n (switch-turn)]
+                       (vec (repeatedly 8 (fn [] (switch-turn))))
+                       ))))))
+
+(reset! board    (vec (repeatedly 8 (fn []
+                     (let [n (switch-turn)]
+                       (vec (repeatedly 8 (fn [] (switch-turn))))
+                       )))))
+
+
+(def coords (for [x (range 1 8) y ["a" "b" "c" "d" "e" "f" "g" "h"]]
+    (str y x)))
+
 
 (defn prepare-board []
-  (map
-   (fn [x] (map (fn [y]
-                 (reset! board (update-in @board [x y]
-                                          ((get-in @board [x y]) (tiles (str x y))))))
-               x)) @board))
+  (vec (map
+     (fn [x] (map (fn [y]
+                   (reset! board (update-in @board [x y]
+                                            (fn [x] ((get-in @board [x y]) (tiles (str x y)))))))
+                 (range 8)
+                 )) (range 8)
+                    )))
 
 (prepare-board)
 
@@ -38,3 +50,11 @@
 (map #(println (map (fn [n] (:color n)) %)) @board)
 
 (map #(println %) board-base)
+
+(print @board)
+
+(.coord ((get-in @board [1 2]) (tiles (str 1 2))))
+
+((fn [n] (get-in @board [1 2])) 1)
+
+(update-in [[:a1 :a2 :a3] [:b1 :b2 :b3] [:c1 :c2 :c3]] [1 2] (fn [n] (get-in @board [1 2])))
