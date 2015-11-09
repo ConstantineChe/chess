cd89f93eeece62561792af436b9903f8ac933652
Konstantyn Cherkasyn <kcherkashyn@ebay.com>
2015-11-09 03:45:18 +0200

    interfaces and move-fns

--- src/chess/model.clj
+++ src/chess/model.clj
@@ -1,38 +1,68 @@
 (ns chess.model)
 
 (defprotocol PTile
-  (setFigure [this fig]))
+  (setFigure [this fig])
+  (getFigure [this]))
+
+(defprotocol Moves
+  (move [this destination])
+  (setOptions [this options]))
+
 
 (deftype Tile [color ^{:volatile-mutable true}figure coord]
   PTile
+
   (setFigure [this fig]
-    (set! figure fig)))
+    (set! figure fig))
 
-(defprotocol Moves
-  (move [this destination]))
+  (getFigure [this]
+    figure))
 
-(deftype Figure [color type ^{:volatile-mutable true}coord]
+(deftype Figure [color type ^{:volatile-mutable true}coord ^{:volatile-mutable true}moves]
   Moves
+
   (move [this destination]
-    (set! coord destination)))
+    (set! coord destination))
+
+  (setOptions [this options]
+    (set! moves options)))
 
 (defn tiles [coord]
-  {:black (new Tile :black :empty coord)
-   :white (new Tile :white :empty coord)})
+  {:black (new Tile :black nil coord)
+   :white (new Tile :white nil coord)})
 
-(defmulti move (fn [fig] (.type fig)))
+(if-let [x (getFigure (get-in @board [1 2]))]
+  x)
 
-(defmethod move :pawn [fig destination]
-  (if (= :empty (get-in @board (update-in 1 (.coord fig) inc)))
-    (do (update-in board (.coord fig) (fn [tile] (.setFigure tile :empty)))
-        (.move fig destination)
-        (update-in board (.coord fig) (fn [tile] (.setFigure tile fig)))
-         switch-turn)))
+(defmulti move-options (fn [fig] (.type fig)))
+
+(defmethod move-options :pawn [fig destination]
+  (if-let (= :empty (get-in @board (update-in 1 (.coord fig) inc)))
+    (.setOptions fig (vec (get-in @board (update-in 1 (.coord fig) inc))))))
+
+(defmulti directions (fn [type] type)
+  )
+
+(defmethod directions :four [pattern]
+  )
+
+(defmethod directions :full [pattern]
+  )
+
+(defmethod directions :pawn
+  )
+
+(defn move [from to]
+  (let [fig (.figure (get-in @board from))]
+    (do (swap! board (.coord fig) assoc-in (.setFigure tile nil))
+        (.move fig to)
+        (swap! board (.coord fig) assoc-in (.setFigure tile fig))
+        switch-turn)))
 
 (def turn (atom :white))
 
 (defn switch-turn [] (if (= @turn :white)
-                       (reset! turnxs :black)
+                       (reset! turn :black)
                        (reset! turn :white)))
 
 (def board
@@ -50,7 +80,7 @@
 
 (def coords
   (vec (map (fn [x] (vec (map (fn [y] (str x y))
-                             ["a" "b" "c" "d" "e" "f" "g" "h"]))) (rever(range 1 9)) )))
+                             ["a" "b" "c" "d" "e" "f" "g" "h"]))) (reverse (range 1 9)) )))
 
 (print coords)
 
@@ -59,9 +89,9 @@
 (defn prepare-board []
   (map
    (fn [x] (vec (map (fn [y]
-                   (reset! board (update-in @board [x y]
-                                            (fn [_] ((get-in @board [x y])
-                                                    (tiles (get-in coords [x y])))))))
+                      (swap! board assoc-in [x y]
+                             ((get-in @board [x y])
+                              (tiles (get-in coords [x y])))))
                  (range 8)))) (range 8)))
 
 (map (fn [x] (map (fn [y] (println (get-in coords [x y] ))) (range 8))) (range 8))
@@ -69,7 +99,6 @@
 (range 8)
 
 (prepare-board)
-
 
 (map #(println (map (fn [n] (str (.coord n) " " (.color n))) %)) @board)
 
